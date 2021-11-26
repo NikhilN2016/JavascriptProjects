9fbf01d849aeac1910fb85e51b5ec26974f25225
NikhilN2016 <95108335+NikhilN2016@users.noreply.github.com>
2021-11-26 17:55:05 -0600

    Create oddOrEven.js

---
+++ OddOrEven/oddOrEven.js
@@ -0,0 +1,12 @@
+//Input a Number
+const number = prompt("Enter any number: ");
+
+//is the number is even
+if(number % 2 == 0) {
+    console.log("Yep this number is even." + number);
+}
+
+// is the number is odd
+else {
+    console.log("Yep this number is odd." + number);
+}
